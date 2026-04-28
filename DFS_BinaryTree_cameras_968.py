43df523c3d93f00e6a35f95cc1f567beea0d5414
Charlee <64256047+CHARALEX279@users.noreply.github.com>
2026-04-27 20:39:08 -0400

    Update DFS_BinaryTree_cameras_968.py

--- DFS_BinaryTree_cameras_968.py
+++ DFS_BinaryTree_cameras_968.py
@@ -1,3 +1,34 @@
+# Definition for a binary tree node.
+#final attempt: 0,1,2 coverage approach with post order DFS
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    cameras = 0
+    #0 == needs camera
+    #1 == give camera
+    #2 == covered
+
+    def DFS(self, node: Optional[TreeNode]) -> int:
+        if node is None:
+            return 2
+        left = self.DFS(node.left)
+        right = self.DFS(node.right)
+        if left == 0 or right == 0: #needs a camera
+            self.cameras += 1
+            return 1
+        if left == 1 or right == 1:
+            return 2
+        return 0
+    def minCameraCover(self, root: Optional[TreeNode]) -> int:
+        #base
+        if self.DFS(root) == 0:
+            self.cameras +=1      
+        return self.cameras
+
+
 #first attempt! i approached it as there's only 1 camera per family, but that was hard to track in DFS. Maybe if i tried BFS and kept track of the levels?
 # Definition for a binary tree node.
 # class TreeNode:
