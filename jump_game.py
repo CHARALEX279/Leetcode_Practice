2e586c00754dfe69365c99b775214670c6d50e49
Charlee <64256047+CHARALEX279@users.noreply.github.com>
2026-08-12 16:01:49 -0400

    Create jump_game.py
    learned solutions

---
+++ jump_game.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        possible_jumps = 0
+        for n in nums:
+            if possible_jumps < 0:
+                return False
+            elif n > possible_jumps:
+                possible_jumps = n
+            possible_jumps -= 1 #we've moved forward at least once, so subtract one
+
+        return True
+#or
+        # max_jumps = 0
+        # for i, n in enumerate(nums):
+        #     if i > max_jumps:
+        #         return False
+        #     max_jumps = max(max_jumps, i+n)
+        # return True
