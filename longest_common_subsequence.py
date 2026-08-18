71253c878fb464e5f65c7fd9bd8aebe21f868fbc
Charlee <64256047+CHARALEX279@users.noreply.github.com>
2026-08-18 15:41:21 -0400

    Update longest_common_subsequence.py

--- longest_common_subsequence.py
+++ longest_common_subsequence.py
@@ -1,6 +1,6 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # recursive attempt. It works, but takes wayyyyy to long.
+        # #recursive attempt. It works, but takes wayyyyy to long.
         # i = 0
         # j = 0
         # count = 0
@@ -13,3 +13,13 @@ class Solution:
         #         return max(LCS(i+1, j), LCS(i, j+i))
         # count += LCS(i,j)
         # return count
+
+
+
+        # #DP approach
+        # iniate a grid of text1 and text2 length
+        # working backard, visit the grid at location i and j
+
+
+        #if i and j are the same value, set that location to 1 + the value of the i+1 square and j+1 square
+        #else, set the currnt dp[i][j] square to the square that is larger, [i+ 1][j] or [i][j+1]
